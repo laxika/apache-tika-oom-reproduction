bef8394a48a5addaadddfa2d8cd7a76c52979933
laxika <laxika91@gmail.com>
2022-08-05 10:25:19 +0200

    Minor reformat.

--- src/main/java/com/example/TikaOOMExample.java
+++ src/main/java/com/example/TikaOOMExample.java
@@ -12,8 +12,6 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class TikaOOMExample {
 
